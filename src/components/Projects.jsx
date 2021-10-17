18d73311a9dfaffa8a32ec65c3aae4d0e8dd16f6
DaTrueBrad <bottbrady@gmail.com>
2021-10-17 13:19:56 -0600

    fixed overflow issue on project cards, updated New U project.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,19 +15,24 @@ function Projects() {
             <p>This app uses Axios GET requests to dig into a REST API which displays select data on the front end for use by the user.</p>
           </div>
           <div className="button-container">
-            <a href="https://github.com/DaTrueBrad/pokemon-team-builder" target="_blank"><i class='bx bx-code-block'></i></a>
-            <a href="https://pokemon-team-and-stats.herokuapp.com/" target="_blank"><i class='bx bx-globe'></i></a>
+            <a href="https://github.com/DaTrueBrad/pokemon-team-builder" target="_blank"><i class='bx bx-code-block'></i>
+            <h4>Code</h4></a>
+            <a href="https://pokemon-team-and-stats.herokuapp.com/" target="_blank"><i class='bx bx-globe'></i>
+            <h4>Site</h4></a>
           </div>
         </div>
 
         <div className="project-card">
           <div>
             <h2>New U Fitness</h2>
-            <img src="./newu.png" alt="" />
-            <p>New U is an online platform for users to center their physical health. Users can create workouts themselves or pick one from a pre-made template made by a top coach, and follow along tracking their progress! If users are not familiar on the ins-and-outs of health and fitness, New U has an articles page where the most informative, simple, and correct articles on the planet will be linked so users can have guidance on their journey.</p>
+            <img src="./newUApp.png" alt="" />
+            <p>New U is an online platform where users can build workout programs, track workouts, browse articles, update user lifting stats, and more - on a mobile-friendly platform.</p>
           </div>
           <div className="button-container">
-            <a href="https://github.com/DaTrueBrad/newU" target="_blank"><i class='bx bx-code-block'></i></a>
+            <a href="https://github.com/DaTrueBrad/newU" target="_blank"><i class='bx bx-code-block'></i>
+            <h4>Code</h4></a>
+            <a href="https://newufitness.herokuapp.com" target="_blank"><i class='bx bx-globe'></i>
+            <h4>Site</h4></a>
           </div>
         </div>
       </div>
